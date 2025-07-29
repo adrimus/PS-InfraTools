f2b78c6d76e65b86fd8f83d21e905eda024b82f6
adrianm <adrian.muscat@systech.net>
2025-07-29 09:55:56 +0200

    Added switch for User and Computer policies to Get-GPPolicyData

--- source/Public/Get-GPPolicyData.ps1
+++ source/Public/Get-GPPolicyData.ps1
@@ -11,35 +11,56 @@ function Get-GPPolicyData {
     Parameter description
     
     .EXAMPLE
-    # Export GPO report in XML format and parse it
-    $path = "C:\Path\To\ChromeGPO.xml"
-    Get-GPOReport -Name "Your GPO Name" -ReportType Xml -Path $path
-    Get-GPPolicyData -XmlPath $path
+    # Get policy data from a GPO by name
+
+    Get-GPPolicyData -GPOName "Your GPO Name"
 
     .EXAMPLE
     # Get all enabled policies
-    $computerPolicies = Get-GPPolicyData -XmlPath "C:\Path\To\ChromeGPO.xml"
+    $computerPolicies = Get-GPPolicyData -GPOName "Your GPO Name"
     $computerPolicies | Where-Object State -eq 'Enabled'
 
     .NOTES
     General notes
     #>
     param (
-        [string]$XmlPath
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GPOName,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Computer", "User", "All")]
+        [string]$Configuration
     )
 
-    [xml]$xmlContent = Get-Content -Path $XmlPath
+    [xml]$xmlContent = Get-GPOReport -Name $GPOName -ReportType Xml
+    
 
     $gponame = $xmlContent.gpo.Name
 
-    $policies = $xmlContent.gpo.Computer.ExtensionData.Extension.Policy
+    $policies = switch ($Configuration) {
+        "Computer" {
+            $xmlContent.gpo.Computer.ExtensionData.Extension.Policy
+        }
+        "User" {
+            $xmlContent.gpo.User.ExtensionData.Extension.Policy
+        }
+        "All" {
+            # No change needed, we will process both Computer and User policies
+            $xmlContent.gpo.Computer.ExtensionData.Extension.Policy + $xmlContent.gpo.User.ExtensionData.Extension.Policy
+        }
+    }
+
+    # display user or computer from the XML path
+
 
     $policyObjects = foreach ($policy in $policies) {
         [PSCustomObject]@{
-            GPOName  = $gponame
-            Name     = $policy.Name
-            State    = $policy.State
-            Category = $policy.Category
+            Configuration = $policy.ParentNode.ParentNode.ParentNode.Name
+            GPOName       = $policy.ParentNode.ParentNode.ParentNode.ParentNode.Name
+            Name          = $policy.Name
+            State         = $policy.State
+            Category      = $policy.Category
         }
     }
 
